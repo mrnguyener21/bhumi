24ee4187d84cd18184d779aa2ac9d153f35696e1
mrnguyener21 <victortnguyen.workemail@gmail.com>
2021-10-14 10:24:33 -0700

    passed sum all numbers in a range

--- FreeCodeCamp/intermediate_algorithm_scripting.js
+++ FreeCodeCamp/intermediate_algorithm_scripting.js
@@ -7,10 +7,18 @@ For example, sumAll([4,1]) should return 10 because sum of all the numbers betwe
 //so im going to have to sort the array to have it least to greatest
 //pretty sure that JS has a range function as well
 //from there probably do like sum(range(array)) or something like that
-let x = [4,1]
+// let theArray = [10,5]
 
-let algo = (a) => {
-    console.log(a)
-}
+// let algo = (arr) => {
+//     let range = arr.sort((a,b) => a-b)
+//     let answer = range[0]
+//     let i = range[0]+1
 
-algo('test')
+//     while (i <= range[1]) {
+//         answer = answer + i
+//         i++
+//     }
+//     return answer
+// }
+// algo(theArray)
+// console.log(algo(theArray))
