56581f8888be6563028aa95380c96352b43bd7e5
mrnguyener21 <victortnguyen.workemail@gmail.com>
2021-10-14 21:30:47 -0700

    solved diff of two sums

--- FreeCodeCamp/intermediate_algorithm_scripting.js
+++ FreeCodeCamp/intermediate_algorithm_scripting.js
@@ -22,3 +22,22 @@ For example, sumAll([4,1]) should return 10 because sum of all the numbers betwe
 // }
 // algo(theArray)
 // console.log(algo(theArray))
+
+//Diff Two Arrays
+/*
+Compare two arrays and return a new array with any items only found in one of the two given arrays, but not both. 
+In other words, return the symmetric difference of the two arrays.
+Note: You can return the array with its elements in any order.
+*/
+//so im going to create a function with two parameters, one for each array
+//in that funtion i need to have a for loop within a for loop
+//the first for loop takes first argument and then loop it through the inner loop which is the comparing array
+//I can probably sort the arrays just to help make it a quicker process
+//OR i can merge them together and remove duplicates
+//is there a remove duplicate function in JS, if not I can just sort and loop through to check
+
+a = ["andesite", "grass", "dirt", "pink wool", "dead shrub"]
+b = ["diorite", "andesite", "grass", "dirt", "dead shrub"]
+
+let difference = a.filter(c => !b.includes(c)).concat(b.filter(c => !a.includes(c)));
+console.log(difference)
