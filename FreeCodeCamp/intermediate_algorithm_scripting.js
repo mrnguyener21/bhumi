fe600d89fda2a4b48d2a55d1001134f3f4c31154
mrnguyener21 <victortnguyen.workemail@gmail.com>
2021-10-18 18:32:41 -0700

    completed challenge: pig latin

--- FreeCodeCamp/intermediate_algorithm_scripting.js
+++ FreeCodeCamp/intermediate_algorithm_scripting.js
@@ -183,3 +183,26 @@ let spinalCase = (str) => {
 }
 
 spinalCase('thisIsSpinalTap');
+
+// Pig Latin
+// Pig Latin is a way of altering English Words. The rules are as follows:
+
+// - If a word begins with a consonant, take the first consonant or consonant cluster, move it to the end of the word, and add ay to it.
+
+// - If a word begins with a vowel, just add way at the end.
+let translatePigLatin = (str) => {
+  let splitStr = str.split('')
+  let vowels = ['a','e','i','o','u']
+  let consonants = []
+
+  if (vowels.includes(splitStr[0])){
+    return str + 'way'
+  }
+  else {
+    for (let i = 0; !vowels.includes(splitStr[i]); i++) consonants.push(splitStr[i])
+    
+    splitStr.splice(0,consonants.length)
+
+    return splitStr.join('') + consonants.join('') + 'ay'
+  }
+}
