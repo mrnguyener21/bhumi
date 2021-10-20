9044c1a21abad7286bb14048f269c0738e484b09
mrnguyener21 <victortnguyen.workemail@gmail.com>
2021-10-19 21:00:44 -0700

    completed challenge sum all primes

--- FreeCodeCamp/intermediate_algorithm_scripting.js
+++ FreeCodeCamp/intermediate_algorithm_scripting.js
@@ -344,3 +344,25 @@ function sumFibs(num) {
   for (let i = 0; i < fibonacciNumbers.length; i++) sum = sum + fibonacciNumbers[i] 
   return sum
 }
+
+// Sum All Primes
+// A prime number is a whole number greater than 1 with exactly two divisors: 1 and itself. For example, 2 is a prime number because it is only divisible by 1 and 2. In contrast, 4 is not prime since it is divisible by 1, 2 and 4.
+
+// Rewrite sumPrimes so it returns the sum of all prime numbers that are less than or equal to num.
+
+function sumPrimes(num) {
+  let prime = [];
+  let sum = 0
+
+  const isPrime = number => {
+  for(let i = 2; i < number; i++)
+    if(number % i === 0) return false;
+    prime.push(number)
+  };
+
+  for (let i = 2; i <= num; i++) isPrime(i)
+  
+  for (let i = 0; i < prime.length; i++) sum = sum + prime[i];
+
+  return sum;
+}
