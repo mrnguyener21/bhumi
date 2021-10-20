8f3cbc5247535733a7efd7195077ebd4b1757ae8
mrnguyener21 <victortnguyen.workemail@gmail.com>
2021-10-19 19:26:16 -0700

    solved challenge: sum all odd fibonacci numbers

--- FreeCodeCamp/basicDataStructure.js
+++ FreeCodeCamp/basicDataStructure.js
@@ -1,1 +1,5 @@
 //add item using splice
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
--- FreeCodeCamp/intermediate_algorithm_scripting.js
+++ FreeCodeCamp/intermediate_algorithm_scripting.js
@@ -307,19 +307,40 @@ pairElement("GCG");
 
 // Check the assertion tests for examples.
 
-function uniteUnique(arr) {
-  let fullArr = [];
+// function uniteUnique(arr) {
+//   let fullArr = [];
 
-  for (let i = 0; i < arguments.length; i++){
-    fullArr.push(arguments[i])
-  };
+//   for (let i = 0; i < arguments.length; i++){
+//     fullArr.push(arguments[i])
+//   };
 
-  fullArr = fullArr.flat()
+//   fullArr = fullArr.flat()
 
-  for (let i = 0; i < fullArr.length; i ++){
-    if (!arr.includes(fullArr[i])){
-      arr.push(fullArr[i])
-    };
-  };
-  return arr
+//   for (let i = 0; i < fullArr.length; i ++){
+//     if (!arr.includes(fullArr[i])){
+//       arr.push(fullArr[i])
+//     };
+//   };
+//   return arr
+// }
+
+
+// Sum All Odd Fibonacci Numbers
+// Given a positive integer num, return the sum of all odd Fibonacci numbers that are less than or equal to num.
+
+// The first two numbers in the Fibonacci sequence are 1 and 1. Every additional number in the sequence is the sum of the two previous numbers. The first six numbers of the Fibonacci sequence are 1, 1, 2, 3, 5 and 8.
+
+// For example, sumFibs(10) should return 10 because all odd Fibonacci numbers less than or equal to 10 are 1, 1, 3, and 5.
+
+function sumFibs(num) {
+  let fibonacciNumbers = [];
+  fibonacciNumbers[0] = 0;
+  fibonacciNumbers[1] = 1;
+
+  for (let i = 2; i <= num; i++) fibonacciNumbers[i] = fibonacciNumbers[i-2] + fibonacciNumbers[i-1];
+  fibonacciNumbers = fibonacciNumbers.filter(e => e%2 !== 0 & e <= num);
+
+  let sum = 0;
+  for (let i = 0; i < fibonacciNumbers.length; i++) sum = sum + fibonacciNumbers[i] 
+  return sum
 }
