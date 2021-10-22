986ca77e752d1f8015be8d0686a5b4b0b44c327a
mrnguyener21 <victortnguyen.workemail@gmail.com>
2021-10-22 15:11:27 -0700

    functional programming: Implement the Filter Method on a Prototype

--- FreeCodeCamp/intermediate_algorithm_scripting.js
+++ FreeCodeCamp/intermediate_algorithm_scripting.js
@@ -662,3 +662,23 @@ filteredList = filteredList.filter(movie => movie.rating >= 8)
 // Only change code above this line
 
 console.log(filteredList);
+
+// Implement the filter Method on a Prototype
+// You might learn a lot about the filter method if you implement your own version of it. It is recommended you use a for loop or Array.prototype.forEach().
+
+// Write your own Array.prototype.myFilter(), which should behave exactly like Array.prototype.filter(). You should not use the built-in filter method. The Array instance can be accessed in the myFilter method using this.
+
+// The global variable
+var s = [23, 65, 98, 5];
+
+Array.prototype.myFilter = function(callback) {
+  // Only change code below this line
+  var newArray = [];
+  for (let i = 0; i < this.length; i++) if (this[i] < 98) newArray.push(this[i])
+  // Only change code above this line
+  return newArray;
+};
+
+var new_s = s.myFilter(function(item) {
+  return item % 2 === 1;
+});
