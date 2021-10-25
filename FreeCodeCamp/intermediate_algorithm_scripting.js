02a0c3ae6a4929502a044a4039e6f21f4e5ff536
mrnguyener21 <victortnguyen.workemail@gmail.com>
2021-10-24 22:59:56 -0700

    imtermediate algorithm scripting: Completed Drop it

--- FreeCodeCamp/intermediate_algorithm_scripting.js
+++ FreeCodeCamp/intermediate_algorithm_scripting.js
@@ -1052,3 +1052,20 @@ function smallestCommons(arr) {
 
 
 smallestCommons([23,18]);
+
+// Drop it
+// Given the array arr, iterate through and remove each element starting from the first element (the 0 index) until the function func returns true when the iterated element is passed through it.
+
+// Then return the rest of the array once the condition is satisfied, otherwise, arr should be returned as an empty array.
+
+function dropElements(arr, func) {
+  for (let i = 0; i < arr.length; i ++){
+    if (func(arr[i]) === true) {
+      arr.splice(0, i)
+      return arr
+    }
+      else if (arr.every(number => func(number) === false)) return []
+  }
+}
+
+dropElements([1, 2, 3, 4], function(n) {return n > 5;});
