fcf14004875f79b8126310b62804dfea2cb8079f
mrnguyener21 <victortnguyen.workemail@gmail.com>
2021-10-25 20:57:39 -0700

    Intermediate Algorithm Scripting: completed Everything Be True

--- FreeCodeCamp/intermediate_algorithm_scripting.js
+++ FreeCodeCamp/intermediate_algorithm_scripting.js
@@ -1108,3 +1108,18 @@ function binaryAgent(str) {
   }
   return translated.join('');
 }
+
+// Everything Be True
+// Check if the predicate (second argument) is truthy on all elements of a collection (first argument).
+
+// In other words, you are given an array collection of objects. The predicate pre will be an object property and you need to return true if its value is truthy. Otherwise, return false.
+
+// In JavaScript, truthy values are values that translate to true when evaluated in a Boolean context.
+
+// Remember, you can access object properties through either dot notation or [] notation.
+
+function truthCheck(collection, pre) {
+  let falsyValues = [false, 0, null, undefined, NaN, '', "", ``];
+  
+  return collection.every(obj => obj.hasOwnProperty(pre) === true & falsyValues.includes(obj[pre]) === false ? true : false)
+}
