e6791e6819db37e0836a7c4e9a71afd73ba12a57
mrnguyener21 <victortnguyen.workemail@gmail.com>
2021-10-25 22:04:39 -0700

    Intermediate Algorithm Scripting: completed Arguments Optional

--- FreeCodeCamp/intermediate_algorithm_scripting.js
+++ FreeCodeCamp/intermediate_algorithm_scripting.js
@@ -1123,3 +1123,26 @@ function truthCheck(collection, pre) {
   
   return collection.every(obj => obj.hasOwnProperty(pre) === true & falsyValues.includes(obj[pre]) === false ? true : false)
 }
+
+// Arguments Optional
+// Create a function that sums two arguments together. If only one argument is provided, then return a function that expects one argument and returns the sum.
+
+// For example, addTogether(2, 3) should return 5, and addTogether(2) should return a function.
+
+// Calling this returned function with a single argument will then return the sum:
+
+// var sumTwoAnd = addTogether(2);
+// sumTwoAnd(3) returns 5.
+
+// If either argument isn't a valid number, return undefined.
+function addTogether() {
+  let args = Array.from(arguments)
+
+  if (args.some( element => typeof(element)!== 'number')){
+    return undefined
+  } else if (args.length === 1){
+    return x => addTogether(args[0],x) // this is the money maker. this is how he was able to get that second argument
+  } else {
+    return args[0] + args[1];
+  }
+}
