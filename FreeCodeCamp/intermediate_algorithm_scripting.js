7429c22e4e58ae09f69cee113cb4a568bbd8b5bc
mrnguyener21 <victortnguyen.workemail@gmail.com>
2021-10-28 13:11:31 -0700

    Intermediate Algorithm: Completed Make a Person

--- FreeCodeCamp/intermediate_algorithm_scripting.js
+++ FreeCodeCamp/intermediate_algorithm_scripting.js
@@ -1141,8 +1141,58 @@ function addTogether() {
   if (args.some( element => typeof(element)!== 'number')){
     return undefined
   } else if (args.length === 1){
-    return x => addTogether(args[0],x) // this is the money maker. this is how he was able to get that second argument
-  } else {
-    return args[0] + args[1];
+      return x => addTogether(args[0],x)
+    } else {
+      return args[0] + args[1];
   }
 }
+
+// Make a Person
+// Fill in the object constructor with the following methods below:
+
+// getFirstName()
+// getLastName()
+// getFullName()
+// setFirstName(first)
+// setLastName(last)
+// setFullName(firstAndLast)
+// Run the tests to see the expected output for each method. The methods that take an argument must accept only one argument and it has to be a string. These methods must be the only available means of interacting with the object.
+const Person = function(firstAndLast) {
+  // Only change code below this line
+  // Complete the method below and implement the others similarly
+  let [firstName, lastName] = firstAndLast.split(' ');
+
+  this.getFirstName = function(){
+    return firstName;
+  };
+
+  this.getLastName = function(){
+    return lastName;
+  };
+
+  this.getFullName = function(){
+    return `${firstName} ${lastName}`;
+
+  };
+
+  this.setFirstName = function(first){
+    firstName = first;
+    return firstName;
+  };
+
+  this.setLastName = function(last){
+    lastName = last;
+    return lastName;
+  };
+  
+  this.setFullName = function(firstAndLast){
+    firstName = firstAndLast.split(' ')[0];
+    lastName = firstAndLast.split(' ')[1];
+    return  `${firstName} ${lastName}`;
+  }
+
+};
+
+const bob = new Person('Bob Ross');
+// console.log(Object.keys(bob).length)
+bob.getFullName();
